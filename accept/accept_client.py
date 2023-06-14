4af7928e4e051e54aedf3b479ce630cf5231e453
Muhammed Atif <muhamedattif@gmail.com>
2023-06-14 03:37:11 +0300

    Add Pre Commit

--- accept/accept_client.py
+++ accept/accept_client.py
@@ -1,10 +1,10 @@
 # Python Standard Library Imports
-from typing import Any, Dict, Union, Tuple
+from typing import Any, Dict, Tuple, Union
 
-from .config import URLsConfig
-from .utils import PaymentSubTypes
 from .accept_connection import AcceptConnection
+from .config import URLsConfig
 from .response_codes import SUCCESS
+from .utils import PaymentSubTypes
 
 
 class AcceptAPIClient:
@@ -20,7 +20,7 @@ class AcceptAPIClient:
     def __init__(self) -> None:
         # Initialize Connection
         self.connection = AcceptConnection()
-        
+
     def create_order(
         self,
         merchant_order_id: str,
@@ -29,7 +29,7 @@ class AcceptAPIClient:
         delivery_needed: bool = False,
         items: list = [],
         shipping_data: dict = {},
-        shipping_details: dict = {}
+        shipping_details: dict = {},
     ) -> Tuple[str, Dict[str, Any], Union[str, None]]:
         """Register an order to Accept's database
 
@@ -41,7 +41,7 @@ class AcceptAPIClient:
             items (list): list of objects contains the contents of the order if it is existing, send it as empty array if it is not available. Defaults to [].
             shipping_data (dict): Mandatory if your order needs to be delivered, otherwise you can delete the whole object. Defaults to {}.
             shipping_details (dict): Mandatory if your order needs to be delivered, otherwise you can delete the whole object. Defaults to {}.
-            
+
 
         Returns:
             Tuple[str, Dict[str, Any], Union[str, None]]: (Code, Order Data Dict, Success/Error Message)
@@ -54,14 +54,14 @@ class AcceptAPIClient:
             "delivery_needed": delivery_needed,
             "items": items,
             "shipping_data": shipping_data,
-            "shipping_details": shipping_details
+            "shipping_details": shipping_details,
         }
 
         code, order_data, message = self.connection.post(
             url=URLsConfig.CREATE_ORDER_URL,
             json=request_body,
         )
-    
+
         # TODO: Validates APIs Return Data Option
         if code == SUCCESS:
             message = "Order Created Successfully"
@@ -75,8 +75,8 @@ class AcceptAPIClient:
         billing_data: dict,
         integration_id: int,
         card_token_key: str = None,
-        expiration: int = 3600, # 1 Hour
-        lock_order_when_paid: bool = False
+        expiration: int = 3600,  # 1 Hour
+        lock_order_when_paid: bool = False,
     ) -> Tuple[str, Union[str, None], Union[str, None]]:
         """Create Payment Key
 
@@ -101,7 +101,7 @@ class AcceptAPIClient:
             "expiration": expiration,
             "integration_id": integration_id,
             "billing_data": billing_data,
-            "lock_order_when_paid": lock_order_when_paid
+            "lock_order_when_paid": lock_order_when_paid,
         }
         if card_token_key:
             request_body["token"] = card_token_key
@@ -110,7 +110,7 @@ class AcceptAPIClient:
             url=URLsConfig.PAYMENT_KEY_URL,
             json=request_body,
         )
-        
+
         # TODO: Validates APIs Return Data Option
         payment_key = None
         if code == SUCCESS:
@@ -119,8 +119,8 @@ class AcceptAPIClient:
         return code, payment_key, message
 
     def get_order(
-        self, 
-        order_id: Union[int, str]
+        self,
+        order_id: Union[int, str],
     ) -> Tuple[str, Dict[str, Any], Union[str, None]]:
         """Retrives Order Data
 
@@ -134,7 +134,7 @@ class AcceptAPIClient:
         code, order_data, message = self.connection.get(
             url=URLsConfig.GET_ORDER_URL.format(order_id=order_id),
         )
-        
+
         # TODO: Validates APIs Return Data Option
         if code == SUCCESS:
             message = "Successfully Retrieved Order: {0} Data: {0}".format(order_id)
@@ -172,7 +172,7 @@ class AcceptAPIClient:
             url=URLsConfig.PAY_URL,
             json=request_body,
         )
-        
+
         # TODO: Validates APIs Return Data Option
         if code == SUCCESS:
             message = "Kiosk or Wallet Payment Processed Successfully"
