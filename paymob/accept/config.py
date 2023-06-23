db64f17ec77d45b9f6d948f6ab91ee50ac64ba6e
Muhammed Atif <muhamedattif@gmail.com>
2023-06-23 21:04:02 +0300

    Start using urljoin

--- paymob/accept/config.py
+++ paymob/accept/config.py
@@ -1,3 +1,6 @@
+# Python Standard Library Imports
+from urllib.parse import urljoin
+
 # Other Third Party Imports
 from decouple import config
 
@@ -13,16 +16,16 @@ class Credentials:
 # URLs Config
 class URLsConfig:
     ACCEPT_BASE_URL = config("PAYMOB_ACCEPT_BASE_URL", default="https://accept.paymob.com")
-    AUTH_TOKEN = ACCEPT_BASE_URL + "/api/auth/tokens"
-    CREATE_ORDER = ACCEPT_BASE_URL + "/api/ecommerce/orders"
-    GET_ORDER = ACCEPT_BASE_URL + "/api/ecommerce/orders/{order_id}"
-    PAYMENT_KEY = ACCEPT_BASE_URL + "/api/acceptance/payment_keys"
-    PAY = ACCEPT_BASE_URL + "/api/acceptance/payments/pay"
-    IFRAME = ACCEPT_BASE_URL + "/api/acceptance/iframes/{iframe_id}?payment_token={payment_key}"
-    GET_TRANSACTION_BY_ID = ACCEPT_BASE_URL + "/api/acceptance/transactions/{transaction_id}"
-    GET_TRANSACTION_BY_MERCHANT_ID = ACCEPT_BASE_URL + "/api/ecommerce/orders/transaction_inquiry"
-    REFUND_TRANSACTION = ACCEPT_BASE_URL + "/api/acceptance/void_refund/refund"
-    VOID_TRANSACTION = ACCEPT_BASE_URL + "/api/acceptance/void_refund/void"
-    CAPTURE_TRANSACTION = ACCEPT_BASE_URL + "/api/acceptance/capture"
-    CREATE_INVOICE_LINK = ACCEPT_BASE_URL + "/api/ecommerce/orders"
-    CREATE_PRODUCT_LINK = ACCEPT_BASE_URL + "/api/ecommerce/products"
+    AUTH_TOKEN = urljoin(ACCEPT_BASE_URL, "api/auth/tokens")
+    CREATE_ORDER = urljoin(ACCEPT_BASE_URL, "api/ecommerce/orders")
+    GET_ORDER = urljoin(ACCEPT_BASE_URL, "api/ecommerce/orders/{order_id}")
+    PAYMENT_KEY = urljoin(ACCEPT_BASE_URL, "api/acceptance/payment_keys")
+    PAY = urljoin(ACCEPT_BASE_URL, "api/acceptance/payments/pay")
+    IFRAME = urljoin(ACCEPT_BASE_URL, "api/acceptance/iframes/{iframe_id}?payment_token={payment_key}")
+    GET_TRANSACTION_BY_ID = urljoin(ACCEPT_BASE_URL, "api/acceptance/transactions/{transaction_id}")
+    GET_TRANSACTION_BY_MERCHANT_ID = urljoin(ACCEPT_BASE_URL, "api/ecommerce/orders/transaction_inquiry")
+    REFUND_TRANSACTION = urljoin(ACCEPT_BASE_URL, "api/acceptance/void_refund/refund")
+    VOID_TRANSACTION = urljoin(ACCEPT_BASE_URL, "api/acceptance/void_refund/void")
+    CAPTURE_TRANSACTION = urljoin(ACCEPT_BASE_URL, "api/acceptance/capture")
+    CREATE_INVOICE_LINK = urljoin(ACCEPT_BASE_URL, "api/ecommerce/orders")
+    CREATE_PRODUCT_LINK = urljoin(ACCEPT_BASE_URL, "api/ecommerce/products")
